ad65a53711f4b8e7cec9e0ae60cee5ecad04a9f7
Steven Avelino <steven.avelino24@outlook.com>
2019-05-06 13:28:16 +0200

    Create test controller and add route to function

---
+++ app/Http/Controllers/PhonemesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PhonemesController extends Controller
+{
+    public function transformPhonemes(Request $request) {
+
+    }
+}

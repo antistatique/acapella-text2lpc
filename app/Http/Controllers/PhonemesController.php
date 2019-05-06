4d46ab5861b19931b6f556e5785c295b1dd96608
Steven Avelino <steven.avelino24@outlook.com>
2019-05-06 13:36:02 +0200

    Add basic call to script to transform to phonemes

--- app/Http/Controllers/PhonemesController.php
+++ app/Http/Controllers/PhonemesController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class PhonemesController extends Controller
 {
     public function transformPhonemes(Request $request) {
+        $process = new Process(['python3', '../transform-phonemes.py', 'hello']);
+        $process->run();
 
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+        
+        dd($process->getOutput());
     }
 }

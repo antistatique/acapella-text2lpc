a749350f407fa71a7c45e00239e31f94362adf0f
Steven Avelino <steven.avelino24@outlook.com>
2019-05-07 16:42:33 +0200

    Define relationships for model Library

--- app/Library.php
+++ app/Library.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Libraries extends Model
 {
-    //
+    /**
+     * Define relationship with User model
+     */
+    public function user() {
+        return $this->hasOne('App\User');
+    }
+
+    /**
+     * Define the many-to-many relationship with Key model
+     */
+    public function keys() {
+        return $this->belongsToMany('App\Key');
+    }
 }

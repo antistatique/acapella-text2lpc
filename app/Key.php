86460864f18f7a435122f2b62788313f562f3060
Steven Avelino <steven.avelino24@outlook.com>
2019-05-07 16:46:03 +0200

    Define relationships in the Key model

--- app/Key.php
+++ app/Key.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Keys extends Model
 {
-    //
+    /**
+     * Define the relationship with the Form model
+     */
+    public function form() {
+        return $this->hasOne('App\Form');
+    }
+
+    /**
+     * Define the relationship with the Position model
+     */
+    public function position() {
+        return $this->hasOne('App\Position');
+    }
+
+    /**
+     * Define the many-to-many relationship with the Library model
+     */
+    public function libraries() {
+        return $this->belongsToMany('App\Library');
+    }
 }

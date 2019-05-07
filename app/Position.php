9640afeb604af54fc2e12593a17a5c249a176b15
Steven Avelino <steven.avelino24@outlook.com>
2019-05-07 16:32:49 +0200

    Define relationships in Position model

--- app/Position.php
+++ app/Position.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Positions extends Model
 {
-    //
+    /*
+    *  Function to define relationship with Key model
+    */
+    public function keys() {
+        return $this->hasMany('App\Key');
+    }
 }

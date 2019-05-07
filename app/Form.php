f0f4d2b766d224d709d8e1f72b5b200e464f5a56
Steven Avelino <steven.avelino24@outlook.com>
2019-05-07 16:48:19 +0200

    Write better comments

--- app/Form.php
+++ app/Form.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Forms extends Model
 {
-    /*
-    *  Function to define relationship with Key model
-    */
+    /**
+     * Define the relationship with the Key model
+     */
     public function keys() {
         return $this->hasMany('App\Key');
     }

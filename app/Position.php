159a164e27607c2ed625f429872c05fb5f910206
Steven Avelino <steven.avelino24@outlook.com>
2019-05-08 08:34:15 +0200

    Define fillable and hidden field in Position model

--- app/Position.php
+++ app/Position.php
@@ -7,6 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Position extends Model
 {
     /**
+     * Define which fields can be fillable by the user
+     */
+    protected $fillable = [
+        'phonemes',
+    ];
+
+    /**
+     * Define which fields are hidden to the user
+     */
+    protected $hidden = [
+        'id',
+    ];
+
+    /**
      * Define the relationship with the Key model
      */
     public function keys() {

8cc228c7390365d00249740c865bcc36af0ef738
Steven Avelino <steven.avelino24@outlook.com>
2019-05-08 11:29:50 +0200

    Create migration for keys table

---
+++ database/migrations/2019_05_08_092726_create_keys_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKeysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keys', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('key')->nullable(false);
+            $table->string('position')->nullable(false);
+            $table->unsignedBigInteger('library_id')->nullable(false);
+            $table->timestamps();
+
+            $table->foreign('library_id')->references('id')->on('libraries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keys');
+    }
+}

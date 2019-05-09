599106f53f3a120a16c7a71aec13e1cde9762ffe
Steven Avelino <steven.avelino24@outlook.com>
2019-05-09 11:22:40 +0200

    Create Key seeder to add default LPC keys

--- database/seeds/KeyTableSeeder.php
+++ database/seeds/KeyTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Key;
 
 class KeyTableSeeder extends Seeder
 {
@@ -11,6 +12,245 @@ class KeyTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Key::create([
+            'key' => 'fantôme',
+            'position' => 'oh_le_lac',
+            'image' => asset('storage/fantôme_oh_le_lac_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'fantôme',
+            'position' => 'bain_bleu',
+            'image' => asset('storage/fantôme_bain_bleau_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'fantôme',
+            'position' => 'pigeon_blanc',
+            'image' => asset('storage/fantôme_pigeon_blanc_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'fantôme',
+            'position' => 'où_est_paul',
+            'image' => asset('storage/fantôme_où_est_paul_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'fantôme',
+            'position' => 'un_zébu',
+            'image' => asset('storage/fantôme_un_zébu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'béni',
+            'position' => 'oh_le_lac',
+            'image' => asset('storage/béni_oh_le_lac_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'béni',
+            'position' => 'bain_bleu',
+            'image' => asset('storage/béni_bain_bleu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'béni',
+            'position' => 'pigeon_blanc',
+            'image' => asset('storage/béni_pigeon_blanc_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'béni',
+            'position' => 'où_est_paul',
+            'image' => asset('storage/béni_où_est_paul_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'béni',
+            'position' => 'un_zébu',
+            'image' => asset('storage/béni_un_zébu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'gare',
+            'position' => 'oh_le_lac',
+            'image' => asset('storage/gare_oh_le_lac_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'gare',
+            'position' => 'bain_bleu',
+            'image' => asset('storage/gare_bain_bleu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'gare',
+            'position' => 'pigeon_blanc',
+            'image' => asset('storage/gare_pigeon_blanc_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'gare',
+            'position' => 'où_est_paul',
+            'image' => asset('storage/gare_où_est_paul_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'gare',
+            'position' => 'un_zébu',
+            'image' => asset('storage/gare_un_zébu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'vos_cases',
+            'position' => 'oh_le_lac',
+            'image' => asset('storage/vos_cases_oh_le_lac_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'vos_cases',
+            'position' => 'bain_bleu',
+            'image' => asset('storage/vos_cases_bain_bleu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'vos_cases',
+            'position' => 'pigeon_blanc',
+            'image' => asset('storage/vos_cases_pigeon_blanc_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'vos_cases',
+            'position' => 'où_est_paul',
+            'image' => asset('storage/vos_cases_où_est_paul_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'vos_cases',
+            'position' => 'un_zébu',
+            'image' => asset('storage/vos_cases_un_zébu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'rance',
+            'position' => 'oh_le_lac',
+            'image' => asset('storage/rance_oh_le_lac_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'rance',
+            'position' => 'bain_bleu',
+            'image' => asset('storage/rance_bain_bleu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'rance',
+            'position' => 'pigeon_blanc',
+            'image' => asset('storage/rance_pigeon_blanc_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'rance',
+            'position' => 'où_est_paul',
+            'image' => asset('storage/rance_où_est_paul_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'rance',
+            'position' => 'un_zébu',
+            'image' => asset('storage/rance_un_zébu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'champignon',
+            'position' => 'oh_le_lac',
+            'image' => asset('storage/champignon_oh_le_lac_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'champignon',
+            'position' => 'bain_bleu',
+            'image' => asset('storage/champignon_bain_bleu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'champignon',
+            'position' => 'pigeon_blanc',
+            'image' => asset('storage/champignon_pigeon_blanc_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'champignon',
+            'position' => 'où_est_paul',
+            'image' => asset('storage/champignon_où_est_paul_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'champignon',
+            'position' => 'un_zébu',
+            'image' => asset('storage/champignon_un_zébu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'fille',
+            'position' => 'oh_le_lac',
+            'image' => asset('storage/fille_oh_le_lac_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'fille',
+            'position' => 'bain_bleu',
+            'image' => asset('storage/fille_bain_bleu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'fille',
+            'position' => 'pigeon_blanc',
+            'image' => asset('storage/fille_pigeon_blanc_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'fille',
+            'position' => 'où_est_paul',
+            'image' => asset('storage/fille_où_est_paul_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'fille',
+            'position' => 'un_zébu',
+            'image' => asset('storage/fille_un_zébu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'des_jupes',
+            'position' => 'oh_le_lac',
+            'image' => asset('storage/des_jupes_oh_le_lac_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'des_jupes',
+            'position' => 'bain_bleu',
+            'image' => asset('storage/des_jupes_bain_bleu_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'des_jupes',
+            'position' => 'pigeon_blanc',
+            'image' => asset('storage/des_jupes_pigeon_blanc_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'des_jupes',
+            'position' => 'où_est_paul',
+            'image' => asset('storage/des_jupes_où_est_paul_default.jpg'),
+            'library_id' => 1,
+        ]);
+        Key::create([
+            'key' => 'des_jupes',
+            'position' => 'un_zébu',
+            'image' => asset('storage/des_jupes_un_zébu_default.jpg'),
+            'library_id' => 1,
+        ]);
     }
 }

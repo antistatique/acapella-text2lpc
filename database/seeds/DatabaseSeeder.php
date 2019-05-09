b77491d2e60400a0c54b95186da5264f4dfb6280
Steven Avelino <steven.avelino24@outlook.com>
2019-05-09 11:23:00 +0200

    Call seeders in base seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            UsersTableSeeder::class,
+            LibraryTableSeeder::class,
+            KeyTableSeeder::class,
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }

58587c3e618b4a66463045f9666a4110f5fbb669
Steven Avelino <steven.avelino24@outlook.com>
2019-05-10 08:40:50 +0200

    Create service for phonemes related methods

---
+++ app/Services/LPC/PhonemeService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services\LPC;
+
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class PhonemeService
+{
+    /**
+     * Method to transform a text to phonemes
+     * 
+     * @return string
+     */
+    public function transform($userText) {
+        $process = new Process(['python3', base_path('phonemizer/transform-phonemes.py'), $userText]);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        return str_replace('-', '', rtrim($process->getOutput()));
+    }
+}

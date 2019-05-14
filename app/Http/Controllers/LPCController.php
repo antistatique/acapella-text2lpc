a145a3b344aea1c92a88bae19d48341db9f84e24
Steven Avelino <steven.avelino24@outlook.com>
2019-05-14 11:08:56 +0200

    Use new LPC service in LPC controller

--- app/Http/Controllers/LPCController.php
+++ app/Http/Controllers/LPCController.php
@@ -6,12 +6,13 @@ use Illuminate\Http\Request;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 use App\Services\LPC\PhonemeService;
+use App\Services\LPC\LPCService;
 
 class LPCController extends Controller
 {
-    public function getLPCKeys(Request $request, PhonemeService $phonemeService) {
+    public function getLPCKeys(Request $request, PhonemeService $phonemeService, LPCService $lpcService) {
         $phonemes = $phonemeService->transform($request->input('sentence'));
-
+        $result = $lpcService->getLPCImages($phonemes, 1);
         return view('phonemes_form', ['phonemes' => $phonemes]);
     }
 }

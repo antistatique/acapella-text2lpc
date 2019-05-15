915422c90177065a466c487455670646f72ebb97
Steven Avelino <steven.avelino24@outlook.com>
2019-05-15 14:56:11 +0200

    Créer footer de base

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,4 +1,19 @@
 <template>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <a href="/">Signaler un problème</a>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    Crée par Antistatique
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <a href="https://a-capella.ch/">Fondation A-Capella</a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -7,6 +22,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    footer {
+        background-color: #702281;
+        color: white;
+        text-align: center;
+        vertical-align: middle;
 
+        & a {
+            color: white;
+        }
+    }
 </style>

15564e5e57c3a72579988e496e33fe15094bd23c
Steven Avelino <steven.avelino24@outlook.com>
2019-05-15 14:06:10 +0200

    Create base blade layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        <div id="app">
+            @yield('content')
+        </div>
+    </body>
+    <script type="text/javascript" src="js/app.js"></script>
+</html>

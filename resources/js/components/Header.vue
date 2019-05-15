4058b34cd26d2f45c626b77c9457fa79bcfdc2ec
Steven Avelino <steven.avelino24@outlook.com>
2019-05-15 14:45:50 +0200

    Create base header

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,4 +1,11 @@
 <template>
+    <header class="container">
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="/">
+                <img id="logo" src="https://a-capella.ch/sites/all/themes/acapella/logo.png" alt="">
+            </a>
+        </nav>
+    </header>
 </template>
 
 <script>
@@ -7,6 +14,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    #logo {
+        max-width: 190px;
+    }
 </style>

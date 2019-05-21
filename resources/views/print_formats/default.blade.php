2dc0301f23c0c882f5b339fe3f01ec3ce1225b22
Steven Avelino <steven.avelino24@outlook.com>
2019-05-21 08:28:07 +0200

    Create view and Sass for the PDF

---
+++ resources/views/print_formats/default.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+
+        <link rel="stylesheet" href="build/css/print_default.css">
+    </head>
+
+    <body>
+        @php
+            $x = 0;
+            $y = 0;
+        @endphp
+        @for ($i = 1; $i <= floor((sizeof($images)/20) + 1); $i++)
+            @while ($x <= (20 * $i) && $x < sizeof($images))
+                @php
+                    $y = $x;
+                    $x += 3;
+                @endphp
+                <section class="tags-row">
+                    @while ($y < sizeof($images) && $y < $x)
+                        <div class="tags-column">
+                            <img src="{{ $images[$y] }}" >
+                        </div>
+                        @php
+                            $y++;
+                        @endphp
+                    @endwhile
+                </section>
+            @endwhile
+            @if ($i < floor((sizeof($images)/20) + 1))
+                <div class="page-break"></div>
+            @endif
+            @php
+                $x++;
+            @endphp
+        @endfor
+    </body>
+</html>

5bbc86edb846f7b0e8868bc52cdb52d40c122936
Steven Avelino <steven.avelino24@outlook.com>
2019-06-03 11:25:04 +0200

    Return error if user didn't input a sentence

--- app/Http/Controllers/LPCController.php
+++ app/Http/Controllers/LPCController.php
@@ -15,12 +15,14 @@ class LPCController extends Controller
     {
         $library = Library::find($request->input('library_id'));
         if ($library->public || (Auth::check() && $library->user_id === Auth::user()->id)) {
-            if ($request->has('sentence')) {
+            if ($request->has('sentence') && $request->input('sentence') != "") {
                 $phonemes = $phonemeService->transform($request->input('sentence'));
                 $lpcKeys = $lpcService->getLPCImages($phonemes, $request->input('library_id'));
     
                 return response()->json(['lpcKeys' => $lpcKeys]);
             }
+
+            return response()->json(['message' => 'Veuillez saisir une phrase']);
         }
 
         return response()->json(['message' => 'Vous n\'êtes pas autorisé à utiliser cette librairie'], 403);

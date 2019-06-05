3af111327a85444811ce434c781a6c2604211caf
Steven Avelino <steven.avelino24@outlook.com>
2019-06-05 11:24:08 +0200

    Add method to create library

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -3,13 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreLibraryRequest;
 use App\Library;
+use Image;
+use Storage;
 
 class LibraryController extends Controller
 {
+    /**
+     * Method to return the view with all the default images
+     */
     public function create(Request $request) {
         $defaultLibrary = Library::where('default', true)->first();
         $keysImages = $defaultLibrary->keys;
         return view('add_library', ['keyImages' => $keysImages]);
+    }
+
+    /**
+     * Method to create a new library
+     */
+    public function store(StoreLibraryRequest $request) {
+        $validated = $request->validated();
+
+        $library = Library::create([
+            'name' => $validated['name'],
+            'public' => $validated['public'],
+            'default' => false,
+            'user_id' => Auth::user()->id,
+        ]);
+
+        return response()->json(['library_id' => $library->id]);
+    }
+
+    public function saveImage(Request $request) {
+        $image = Image::make($request->input('image_base64'));
+        $image->save(storage_path() . '/app/temp_images/test_original.png');
     }
 }

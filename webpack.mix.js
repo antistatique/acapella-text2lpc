87288344f8cfb9015ddb50de7ac1886076fa6e7f
Steven Avelino <steven.avelino24@outlook.com>
2019-06-12 08:20:11 +0200

    Remove versioning of assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/build/js').version()
-   .sass('resources/sass/app.scss', 'public/build/css').version();
+mix.js('resources/js/app.js', 'public/build/js')
+   .sass('resources/sass/app.scss', 'public/build/css');

21aee2e253ea15ce6fadbd39bccf71777e749b21
Steven Avelino <steven.avelino24@outlook.com>
2019-10-09 17:01:21 +0200

    Create view for libraries listing

---
+++ resources/js/views/Libraries.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <div class="container mt-5">
+      <div class="page-header text-center">
+        <h1>
+          Mes bibliothèques
+        </h1>
+      </div>
+      <div class="row justify-content-center mx-auto mt-5">
+        <div
+          v-for="library of JSON.parse(libraries)"
+          :key="library.id"
+          class="col-md-4 col-sm-12"
+        >
+          <card-library :library="library" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import CardLibrary from '../components/CardLibrary'
+
+export default {
+    components: {
+        CardLibrary
+    },
+    props: {
+        libraries: {
+            default: '',
+            type: String
+        }
+    },
+    mounted() {
+        console.log(JSON.parse(this.libraries))
+    }
+}
+</script>
+
+<style>
+
+</style>
+

ee83a41dde854d4c12701283d19b30f6a6b05796
Steven Avelino <steven.avelino24@outlook.com>
2019-10-17 15:18:41 +0200

    Create base modal for phonemes customising

---
+++ resources/js/components/PhonemesModal.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-toggle="modal"
+      data-target="#phonemesModal"
+    >
+      Modifier résultat (phonèmes)
+    </button>
+    <div
+      id="phonemesModal"
+      class="modal fade"
+      tabindex="-1"
+      role="dialog"
+      aria-hidden="true"
+    >
+      <div
+        class="modal-dialog modal-lg modal-dialog-centered"
+        role="document"
+      >
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">
+              Modification des phonèmes
+            </h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-12">
+                <textarea
+                  v-model="phonemes_"
+                  class="form-control"
+                  cols="30"
+                  rows="5"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Annuler
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary"
+            >
+              Modifier le résultat
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    phonemes: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      phonemes_: this.phonemes
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
